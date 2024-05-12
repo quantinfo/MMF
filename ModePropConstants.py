bafecd6b8e9d69dad9ef7e1221598e82251707f8
dj-gauthier <46763378+dj-gauthier@users.noreply.github.com>
2024-05-12 09:34:43 -0400

    different models for attenuation

--- ModePropConstants.py
+++ ModePropConstants.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 plot_beta = 'n'  # plot propagation constant vs. mode number
 plot_beta_dif = 'y' # plot the difference in propagation constants
-plot_single_mode = 'n' # plot a transverse profile of a single mode
+plot_single_mode = 'y' # plot a transverse profile of a single mode
 plot_sum_no_abs = 'y' # plot transverse profile with random phases, no absorption
 plot_sum_abs = 'y' # plot transverse profile with random phases, mode-dependent absorption
 
@@ -19,7 +19,7 @@ plot_sum_abs = 'y' # plot transverse profile with random phases, mode-dependent 
 NA = 0.22
 radius = 100. # in microns
 areaSize = 2.5*radius # 3.5*radius # calculate the field on an area larger than the diameter of the fiber
-npoints = 2**7 # resolution of the window
+npoints = 2**8 # resolution of the window
 n1 = 1.45
 wl = 1.55 # wavelength in microns
 
@@ -45,7 +45,7 @@ print(f"Estimated number of modes using the V number = {Nmodes_estim}")
 
 modes_semianalytical = solver.solve(mode = 'SI', curvature = None)
 
-n_modes = len(modes_semianalytical.betas)
+n_modes = modes_semianalytical.number
 plt_pts = npoints**2
 
 # Sort the modes
@@ -80,7 +80,7 @@ if (plot_beta_dif == 'y'):
     plt.show()
     
 if (plot_single_mode == 'y'):
-    imode = 10
+    imode = 6
     plt.imshow(np.abs(modes['SA']['profiles'][imode].reshape([npoints]*2)))
     plt.show()
 
@@ -101,16 +101,30 @@ if (plot_sum_no_abs == 'y'):
 print('sum elements no absorption ',np.sum(np.abs(mode_sum)**2)/n_modes)
 
 # absorption term
-gamma = 10.e4   # in units of m^{-2}
+#gamma = 2.e16   # in units of m^{-2}
 length = 75.  # in units of m
 
 # find the imaginary part of the propagation constant for each mode
+absorption = np.zeros(n_modes)
+alpha = 1.e-1 # m^{-1}
+n_max = round(n_modes*.7)
 beta_i = np.zeros(n_modes) # save it in case it is needed later
+#beta_diff = np.zeros(n_modes)
 mode_sum_abs = np.zeros(plt_pts,dtype=np.complex_)
+#A = 1.e-8 # linear
+#A = 1.e-10
 
 for i in range(n_modes):
-    beta_i[idx[i]] = gamma/(modes_semianalytical.betas[idx[i]]*1.e6) # convert betas to units of m^{-1}
+    beta_i[idx[i]] = gamma*(((1./(modes_semianalytical.betas[idx[i]]*1.e6))-(1./(modes_semianalytical.betas[idx[0]]*1.e6)))**2) # convert betas to units of m^{-1}
     mode_sum_abs += np.exp(-length*beta_i[idx[i]])*phase_exp[idx[i]]*modes_semianalytical.profiles[idx[i]]
+    #beta_diff[idx[i]] = (modes_semianalytical.betas[idx[0]]-modes_semianalytical.betas[idx[i]])*1.e6 
+    #beta_diff[idx[i]] = ((modes_semianalytical.betas[idx[0]]-modes_semianalytical.betas[idx[i]])**2)*1.e12
+    #mode_sum_abs += np.exp(-length*A*beta_diff[idx[i]])*phase_exp[idx[i]]*modes_semianalytical.profiles[idx[i]]
+    #if idx[i] < n_max:
+    #    absorption[idx[i]] = 0.
+    #else:
+    #    absorption[idx[i]] = alpha
+    #mode_sum_abs += np.exp(-length*absorption[idx[i]])*phase_exp[idx[i]]*modes_semianalytical.profiles[idx[i]]
 
 print('sum elements with absorption ',np.sum(np.abs(mode_sum_abs)**2)/n_modes)
 
